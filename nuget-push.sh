7e6a84507b3678405c106eaae2f741b11cd62006
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-16 12:33:58 +0100

    SKipper push om det er en PR

--- nuget-push.sh
+++ nuget-push.sh
@@ -1,6 +1,8 @@
 betaSuffix="beta"
 masterSuffix="master"
-if [[ ${TRAVIS_BRANCH} == $betaSuffix ]] || [[ ${TRAVIS_BRANCH} == $masterSuffix ]];then
+if [[ $TRAVIS_PULL_REQUEST != false ]];then
+	echo "This is a pull-request build, skipping deploy."
+if [[ $TRAVIS_BRANCH == $betaSuffix ]] || [[ $TRAVIS_BRANCH == $masterSuffix ]];then
 	echo "Is on beta or master branch, deploying Nuget package ..."
 	mono nuget.exe push *.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json
 else

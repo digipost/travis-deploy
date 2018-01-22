b69f16704fa6fbb1f94299db50728f876008f752
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-22 15:31:21 +0100

    Add script for adding secrets

---
+++ add-secrets.sh
@@ -0,0 +1,5 @@
+cd Digipost.Signature.Api.Client.Core
+
+# Add certificate info to user-secrets
+dotnet user-secrets set Certificate:Path:Absolute "${PWD}/Bring_Digital_Signature_Key_Encipherment_Data_Encipherment.p12"
+dotnet user-secrets set Certificate:Password $BRING_CERTIFICATE_PASSWORD >/dev/null 2>&1

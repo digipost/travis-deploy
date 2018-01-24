731610bbd19c7b488c06506315852393a9ffeec1
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-24 14:37:09 +0100

    Do not patch if not on master or beta branch

--- nuspec-version-patcher.sh
+++ nuspec-version-patcher.sh
@@ -6,8 +6,6 @@
 # 5: Patch the .nuspec file.
 nuspecFile=$1
 
-TRAVIS_BUILD_NUMBER=22
-
 function stop_if_no_assembly_version_found {
 	if [[ ${#lineWithVersionNumber} -eq "0" ]];then
 		echo "Did not find assembly version with version patcher. Please check that patcher is installed correctly and that it can find the assembly version files. Exiting!" >&2 #Echo and send to stderr
@@ -22,23 +20,18 @@ lineWithVersionNumber=$(mono ./Zero29.1.0.0/tools/Zero29.exe -l | head -n 1)
 if [[ ${TRAVIS_BRANCH} == "master" ]];then
  	fullAssemblyVersion=$(echo $lineWithVersionNumber | egrep -o '([0-9].){3}([0-9])')  # Ex. 4.0.0.0 (Keep all parts if on master)
  	stop_if_no_assembly_version_found
+	
 	echo "Is on master branch and found full version number (Major.Minor.Patch.Build) to be ${fullAssemblyVersion}."
-else	
+elif [[ ${TRAVIS_BRANCH} == "beta" ]];then
  	assemblyVersion=$(echo $lineWithVersionNumber | egrep -o '([0-9].){2}([0-9])') 		# Ex. 4.0.0   (Remove build number and replace it with travis build number)
 	stop_if_no_assembly_version_found
-	fullAssemblyVersion="${assemblyVersion}.${TRAVIS_BUILD_NUMBER}"
-	echo "Is not on master branch and parsed version without build number (Major.Minor.Patch) to be ${assemblyVersion}, and appended TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER, resulting in $fullAssemblyVersion. "
+	fullAssemblyVersion="${assemblyVersion}.${TRAVIS_BUILD_NUMBER}-beta"
+	
+	echo "Is on beta branch and parsed version without build number (Major.Minor.Patch) to be ${assemblyVersion}.\
+ Appended TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER and result is $fullAssemblyVersion. "
+else
+	echo "Is not on beta or master branch. No need to patch .nuspec file!"
 fi
-
-
-betaSuffix="beta"
-if [[ ${TRAVIS_BRANCH} == $betaSuffix ]];then
-	echo "Is on beta branch, setting '-${betaSuffix}' as package suffix."
-	assemblyVersion+="-${betaSuffix}"
-fi
-
-echo "Patching version in .nuspec to '${assemblyVersion}'!"
 
 #Patch assembly version number in .nuspec
-sed -i.originalfilebackup "s/VERSION_PLACEHOLDER/${assemblyVersion}/g" $nuspecFile
-
+sed -i.originalfilebackup "s/VERSION_PLACEHOLDER/${fullAssemblyVersion}/g" $nuspecFile

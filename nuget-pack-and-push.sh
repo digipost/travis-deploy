2a4132f4912b6127ae212584015f9395186c0e2a
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-08 14:34:56 +0200

    Deployer uten mono

--- nuget-pack-and-push.sh
+++ nuget-pack-and-push.sh
@@ -2,8 +2,8 @@ if [[ $TRAVIS_PULL_REQUEST != false ]];then
 	echo "This is a pull-request build, skipping pack and deploy."
 elif [[ $TRAVIS_BRANCH == "beta" ]] || [[ $TRAVIS_BRANCH == "master" ]];then
 	echo "Is on beta or master branch, packing and deploying Nuget package ..."
-	mono nuget.exe pack -Verbosity detailed
-	mono nuget.exe push *.nupkg -Verbosity detailed -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json
+	dotnet pack
+	dotnet nuget push signature-api-client.nuspec --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 else
 	echo "Is not on beta or master branch, skipping pack and deploy."
 fi

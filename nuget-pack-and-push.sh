7771c08971db5f0e26004288d5c8ae8b09644743
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-10 15:21:55 +0200

    Forenkler printout og fjerner bug i branch-test

--- nuget-pack-and-push.sh
+++ nuget-pack-and-push.sh
@@ -4,9 +4,11 @@ nuspecFile=$1
 currentBranch=$2
 apiKey=$3
 
+echo "Will try to push nuget package based on .nuspec file '$nuspecFile' from branch '$currentBranch' with api-key '$apiKey'"
+
 if [[ false != false ]];then
 	echo "This is a pull-request build, skipping pack and deploy."
-elif [[ $currentBranch != "beta" ]] || [[ $currentBranch == "master" ]];then
+elif [[ $currentBranch == "beta" ]] || [[ $currentBranch == "master" ]];then
 	echo "Is on beta or master branch, packing and deploying Nuget package ..."
 	nuget pack $nuspecFile
 	dotnet nuget push ../ --source https://api.nuget.org/v3/index.json --api-key $apiKey

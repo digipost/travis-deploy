7a83a28e180c3d32c69491c1b0736943f9631c8b
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-17 12:33:21 +0200

    Fikser nuspec-version-patching
    - Det er nå bare version-patcher. Gjør ikke noe med nuspec-fil.
    - Fungerer ikke lokalt, dessverre. Dette må fikses.

--- version-patcher.sh
+++ version-patcher.sh
@@ -5,11 +5,11 @@
 # 3: Add -beta suffix if applicable
 # 4: Patch the .nuspec file.
 
-nuspecFile=$1
+buildProperties=$1
 currentBranch=$2
 buildNumber=$3
 
-echo "Starting to patch nuspec file '${nuspecFile}'. Is on branch '${currentBranch}' and build number is '${buildNumber}'"
+echo "Starting to patch version in '${buildProperties}'. Is on branch '${currentBranch}' and build number is '${buildNumber}'"
 
 function stop_if_no_assembly_version_found {
 	if [[ ${#lineWithVersionNumber} -eq "0" ]];then
@@ -18,22 +18,22 @@ function stop_if_no_assembly_version_found {
 	fi
 }
 
-lineWithVersionNumber=$(cat SolutionItems/AssemblyInfo.cs | grep AssemblyVersion)
+lineWithVersionNumber=$(cat ${buildProperties} | grep Version)
 
-echo "Fetched line with current base version from AssemblyInfo.cs: '" + $lineWithVersionNumber + "'";
+echo "Fetched line with current base version from AssemblyInfo.cs: ' ${lineWithVersionNumber} '";
 
 if [[ ${currentBranch} == "master" ]];then
- 	fullAssemblyVersion=$(echo $lineWithVersionNumber | egrep -o '([0-9].){3}([0-9])')  # Ex. 4.0.0.0 (Keep all parts if on master)
+ 	fullAssemblyVersion=$(echo ${lineWithVersionNumber} | egrep -o '([0-9].){3}([0-9])')  # Ex. 4.0.0.0 (Keep all parts if on master)
  	stop_if_no_assembly_version_found
 	
 	echo "Is on master branch and found full version number (Major.Minor.Patch.Build) to be ${fullAssemblyVersion}."
 elif [[ ${currentBranch} == "beta" ]];then
- 	assemblyVersion=$(echo $lineWithVersionNumber | egrep -o '([0-9].){2}([0-9])') 		# Ex. 4.0.0   (Remove build number and replace it with input build number)
+ 	assemblyVersion=$(echo ${lineWithVersionNumber} | egrep -o '([0-9].){2}([0-9])') 		# Ex. 4.0.0   (Remove build number and replace it with input build number)
 	stop_if_no_assembly_version_found
 	fullAssemblyVersion="${assemblyVersion}.${buildNumber}-beta"
 	
-	echo "Is on beta branch and parsed version without build number (Major.Minor.Patch) to be ${assemblyVersion}.\
- Appended build number=$buildNumber and result is $fullAssemblyVersion. "
+	echo "Is on beta branch and parsed version without build number (Major.Minor.Patch) to be '${assemblyVersion}'.\
+ Appended build number=$buildNumber and result is '$fullAssemblyVersion'. "
 else
 	echo "Is not on beta or master branch. No need to patch .nuspec file!"
 	exit 0

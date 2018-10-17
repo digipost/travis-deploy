c3be1cfcbe4245563ae1ef345465711bb934f966
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-17 12:22:32 +0200

    Refaktorerer pack- og push-script
    - Tar inn prosjektene som skal pakkes
    - Pakker til egen mappe
    - Pusher røkla
    - Feiler raskt om noe feiler for å ikke få bad state
    
    Husk at denn

--- nuget-pack-and-push.sh
+++ nuget-pack-and-push.sh
@@ -1,17 +1,46 @@
 #!/usr/bin/env bash
 
-nuspecFile=$1
 currentBranch=$2
 apiKey=$3
+projectRoot=$4
+projectDirectories=$5
 
-echo "Will try to push nuget package based on .nuspec file '$nuspecFile' from branch '$currentBranch' with api-key '$apiKey'"
+function fail_fast_if_one_fails(){
+	set -e #Sets fast fail if one of the following fails 
+}
 
-if [[ false != false ]];then
+function pack(){
+    projectName=$1
+    
+    echo "Packing '$projectName' with output $projectRoot"
+    dotnet pack $projectName --output $projectRoot/packed
+}
+
+function push(){
+    fileName=$1    
+  	dotnet nuget push $fileName --source https://api.nuget.org/v3/index.json --api-key $apiKey
+}
+
+echo "Will try to push nuget packages for branch '$currentBranch' with api-key '$apiKey' and root directory '$projectRoot' and project directories: '$projectDirectories'".
+
+if [[ false != false ]];then # TODO: Dette må settes til $TRAVIS_PULL_REQUEST != false når ferdig med hacking
 	echo "This is a pull-request build, skipping pack and deploy."
 elif [[ $currentBranch == "beta" ]] || [[ $currentBranch == "master" ]];then
 	echo "Is on beta or master branch, packing and deploying Nuget package ..."
-	nuget pack $nuspecFile
-	dotnet nuget push ../ --source https://api.nuget.org/v3/index.json --api-key $apiKey
+	echo "Trying to pack from directory: $projectRoot."
+	
+    fail_fast_if_one_fails
+           
+    for project in $projectDirectories
+    do
+        pack $project
+    done
+    
+    for entry in "$projectRoot/packed"/*
+    do
+        push $entry
+    done
+	
 else
 	echo "Is not on beta or master branch, skipping pack and deploy."
-fi
+fi      

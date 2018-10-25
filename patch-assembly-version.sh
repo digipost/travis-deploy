9520f3f67249d906856733b987bcb5916081cfba
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-25 16:11:34 +0200

    Full opprydding i hvordan vi beregner og printer versjoner
    Mest for lesbarhet og litt fordi vi hadde bugget det til med
    ugyldig versjonsnummer for AssemblyVersion

--- patch-assembly-version.sh
+++ patch-assembly-version.sh
@@ -7,40 +7,64 @@ buildNumber=$3
 echo "Starting to patch version in '${buildPropertiesFile}'. Is on branch '${currentBranch}' and build number is '${buildNumber}' ..."
 
 function stop_if_no_assembly_version_found {
-	if [[ ${#lineWithVersionNumber} -eq "0" ]];then
+    version=$1
+
+	if [[ ${version} -eq "0" ]];then
 		echo "Did not find assembly version with version patcher. Please check that patcher is installed correctly and that it can find the assembly version files. Exiting!" >&2 #Echo and send to stderr
 		exit 1 # terminate and indicate error
 	fi
+}
+
+function print_versions {
+    baseVersion=$1
+    assemblyVersion=$2
+    nugetVersion=$3
+
+    echo "======================================================================"
+	echo " Base version before patching: $baseVersion"
+	echo " Assembly Version/.dll (AssemblyVersion): $assemblyVersion"
+	echo " Nuget version (Version): $nugetVersion"
+	echo "======================================================================"
 }
 
 lineWithVersionNumber=$(cat ${buildPropertiesFile} | grep -m1 Version)
 
 echo "Fetched line with current base version from AssemblyInfo.cs: ' ${lineWithVersionNumber} '";
 
+baseVersionThreeTuple=$(echo ${lineWithVersionNumber} | egrep -o '([0-9].){2}([0-9])') 		# Ex. 4.0.0   (Remove build number and replace it with input build number)
+baseVersionFourTuple=$(echo ${lineWithVersionNumber} | egrep -o '([0-9].){3}([0-9])')       # Ex. 4.0.0.0 (Keep all parts if on master)
+
 if [[ ${currentBranch} == "master" ]];then
- 	fullAssemblyVersion=$(echo ${lineWithVersionNumber} | egrep -o '([0-9].){3}([0-9])')  # Ex. 4.0.0.0 (Keep all parts if on master)
- 	stop_if_no_assembly_version_found
+ 	assemblyVersion=${baseVersionFourTuple} 
+ 	nugetVersion=${assemblyVersion}
+ 	
+ 	stop_if_no_assembly_version_found ${assemblyVersion}
+ 	
+ 	print_versions ${baseVersionFourTuple} ${assemblyVersion} ${nugetVersion}
+ 	
 	
-	echo "Parsed full version number (Major.Minor.Patch.Build) to be ${fullAssemblyVersion}."
+	echo "Parsed full version number (Major.Minor.Patch.Build) to be ${assemblyVersion}."
 elif [[ ${currentBranch} == "beta" ]];then
- 	assemblyVersion=$(echo ${lineWithVersionNumber} | egrep -o '([0-9].){2}([0-9])') 		# Ex. 4.0.0   (Remove build number and replace it with input build number)
-	stop_if_no_assembly_version_found
-	fullAssemblyVersion="${assemblyVersion}.${buildNumber}-beta"
 	
-	echo "Parsed version without build number (Major.Minor.Patch) to be '${assemblyVersion}'. Appended build number=$buildNumber and result is '$fullAssemblyVersion'. "
+	assemblyVersion="${baseVersionThreeTuple}.${buildNumber}"
+	nugetVersion="${baseVersionThreeTuple}.${buildNumber}-beta"
 	
+	stop_if_no_assembly_version_found $assemblyVersion
+
+ 	print_versions ${baseVersionFourTuple} ${assemblyVersion} ${nugetVersion}
+
 else
 	echo "Is not on beta or master branch. No need to patch $buildPropertiesFile!"
 	exit 0
 fi
 
-sed -i.backup "s/$assemblyVersion/$fullAssemblyVersion/g" $buildPropertiesFile
+sed -i.backup "s/\<Version\>$baseVersionFourTuple\<\/Version\>/\<Version\>$nugetVersion\<\/Version\>/g" ${buildPropertiesFile}
+sed -i.backup "s/\<AssemblyVersion\>$baseVersionFourTuple\<\/AssemblyVersion\>/\<AssemblyVersion\>$assemblyVersion\<\/AssemblyVersion\>/g" ${buildPropertiesFile}
 rm -r *.backup
 
-echo "======================================================================"
+echo "Version patched successfully."
+
 echo " Patched build properties file: "
-echo "======================================================================"
 
 cat $buildPropertiesFile
 
-echo "Version patched successfully."

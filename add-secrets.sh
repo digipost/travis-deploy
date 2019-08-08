d334d9c3905a0304dff359ff419611378963f6a7
AaronArrick <arrickaaron@gmail.com>
2019-08-08 12:48:38 +0200

    Change add-secrets.sh to be generic, allowing it to be used across different projects.

--- add-secrets.sh
+++ add-secrets.sh
@@ -1,5 +1,10 @@
-cd Digipost.Signature.Api.Client.Core
+#!/usr/bin/env bash
+
+rootProject=$1
+certFileName=$2
+
+cd $rootProject
 
 # Add certificate info to user-secrets
-dotnet user-secrets set Certificate:Path:Absolute "${TRAVIS_BUILD_DIR}/Bring_Digital_Signature_Key_Encipherment_Data_Encipherment.p12"
+dotnet user-secrets set Certificate:Path:Absolute "${TRAVIS_BUILD_DIR}/$certFileName"
 dotnet user-secrets set Certificate:Password $BRING_CERTIFICATE_PASSWORD >/dev/null 2>&1
